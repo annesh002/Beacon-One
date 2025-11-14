553cb222420a55ea54316e0b13616ae2117215f0
Annesh Chatterjee <annesh515@gmail.com>
2025-11-14 13:13:14 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,38 @@
+const navToggle = document.getElementById("navToggle");
+const navMenu = document.getElementById("navMenu");
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+    navToggle.classList.toggle("active");
+});
+
+
+const avatars = document.querySelectorAll(".avatar");
+const prevBtn = document.querySelector(".arrow-btn.prev");
+const nextBtn = document.querySelector(".arrow-btn.next");
+
+let current = 0;
+
+function setActive(i) {
+  avatars.forEach(a => a.classList.remove("active"));
+  avatars[i].classList.add("active");
+}
+
+prevBtn.addEventListener("click", () => {
+  current = (current - 1 + avatars.length) % avatars.length;
+  setActive(current);
+});
+
+nextBtn.addEventListener("click", () => {
+  current = (current + 1) % avatars.length;
+  setActive(current);
+});
+
+avatars.forEach((avatar, i) =>
+  avatar.addEventListener("click", () => {
+    current = i;
+    setActive(current);
+  })
+);
+
+setActive(current);
